0a4b084a1300c87d94fc68fed3f8a7892cdd5ad8
Antonio Correia <antonio.alves.correia@gmail.com>
2022-03-08 12:31:55 -0300

    removing sample stuff

--- BusinessCardScan.API/BusinessCardScan.API/Program.cs
+++ BusinessCardScan.API/BusinessCardScan.API/Program.cs
@@ -18,26 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.MapPost("/ProcessCard",
+app.MapPost("/processcard",
     async Task<IResult> (HttpRequest request) =>
     {
         if (!request.HasFormContentType)
@@ -53,15 +35,8 @@ app.MapPost("/ProcessCard",
 
         BusinessCardRecognizer rec = new BusinessCardRecognizer(builder.Configuration);
         var businessCard = await rec.ProcessBusinessCardAsync(stream);
-        //var reader = new StreamReader(stream);
-        //var text = await reader.ReadToEndAsync();
-
+       
         return Results.Ok(businessCard);
     });
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
